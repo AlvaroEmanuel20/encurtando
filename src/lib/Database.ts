d1c6e048ff05f2dd855a3458a5b710c99cb5c975
Álvaro Emanuel <alvarodiasribeiro16@gmail.com>
2023-03-06 09:13:13 -0300

    change: database for mongodb atlas

--- src/lib/Database.ts
+++ src/lib/Database.ts
@@ -3,7 +3,9 @@ import mongoose from "mongoose";
 export default class Database {
 	async connect() {
 		try {
-			await mongoose.connect(`${process.env.DB_URL}${process.env.DB_PORT}`);
+			await mongoose.connect(
+                `mongodb+srv://${process.env.MONGODB_ATLAS_USER}:${process.env.MONGODB_ATLAS_PASS}@cluster0.yfgkpdj.mongodb.net/?retryWrites=true&w=majority`
+            );
 			console.log("Connected to database 👋");
 		} catch (error) {
 			throw new Error("Error connect to database");
